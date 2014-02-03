e55e742610cf94845fc2348a4c49029256022733
peltho <boitutile@gmail.com>
2014-02-03 15:03:00 +0100

    WIP

--- partenaires.tpl.php
+++ partenaires.tpl.php
@@ -5,3 +5,12 @@
  * @subpackage MIAGE
  * @since MIAGE 1.0
  */
+get_header();
+$toRender['template_directory'] = get_template_directory_uri();
+$toRender['title'] = get_the_title();
+
+// TODO
+
+$twig = initTwig('');
+echo $twig->render('partenaires.tpl.twig', array('render'=>$toRender));
+get_footer();
